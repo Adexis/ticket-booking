aaf26e701f088b8de8900841da652591da0c9d3e
adamw <adamw003@gmail.com>
2019-05-24 04:39:55 +0200

    Added Constants for Repositories

---
+++ src/main/java/com/awalczak/ticketbooking/validators/Constants.java
@@ -0,0 +1,8 @@
+package com.awalczak.ticketbooking.validators;
+
+class Constants {
+
+    static final String REGEX_ANY_UPPER_CASE_CHARACTER_WITH_POLISH = "A-ZĄĆĘŁŃÓŚŹŻ";
+    static final String REGEX_ANY_LOWER_CASE_CHARACTER_WITH_POLISH = "a-ząćęłńóśźż";
+    static final int MINIMUM_MINUTES_BEFORE_SCREENING = 15;
+}
